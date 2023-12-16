a6dc435ceb5899e6759b4d01662c6c4c43788421
AdmBeck <admbeck@outlook.com>
2023-12-16 21:33:47 +0500

    removed help

--- config.h
+++ config.h
@@ -24,7 +24,7 @@ static const Block blocks[] = {
 	{"",	"sb-clock",	60,	1},
 	{"",	"sb-internet",	5,	4},
 	/* {"",	"sb-iplocate", 0,	27}, */
-	{"",	"sb-help-icon",	0,	15},
+	/* {"",	"sb-help-icon",	0,	15}, */
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
